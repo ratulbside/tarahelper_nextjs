dc207d5eb482d022b1fd307fdbb2c03ed62e3728
Ratul <ratul.bside@gmail.com>
2024-02-28 10:06:48 +0600

    Implement sign in page.

---
+++ app/components/form.tsx
@@ -0,0 +1,49 @@
+export function Form({
+    action,
+    children,
+  }: {
+    action: any;
+    children: React.ReactNode;
+  }) {
+    return (
+      <form
+        action={action}
+        className="mt-8 space-y-6 px-4 py-8 sm:px-16"
+      >
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email Address
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="user@companay.com"
+            autoComplete="email"
+            required
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          />
+        </div>
+        {children}
+      </form>
+    );
+  }
+  
